46299122ca6a6db6326a8dfff550c7939fb89cd7
lucaszebre <yakirisk@gmail.com>
2023-09-07 17:48:42 +0200

    add the TypeORm export missing

--- src/reservations/reservations.module.ts
+++ src/reservations/reservations.module.ts
@@ -1,9 +1,14 @@
+/* eslint-disable prettier/prettier */
 import { Module } from '@nestjs/common';
 import { ReservationsService } from './reservations.service';
 import { ReservationsController } from './reservations.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Reservation } from './entities/reservation.entity';
 
 @Module({
+  imports:[TypeOrmModule.forFeature([Reservation])],
   controllers: [ReservationsController],
   providers: [ReservationsService],
+  exports:[ReservationsModule]
 })
 export class ReservationsModule {}
