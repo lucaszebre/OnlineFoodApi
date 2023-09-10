357e9f0b3443ee570510323782c77a17b87c916a
lucaszebre <yakirisk@gmail.com>
2023-09-10 11:19:20 +0200

    add RolesGuard to products routes

--- README.md
+++ README.md
@@ -8,10 +8,7 @@ tecture. It's richly built with a simple scientific technique and best practices
 - add, edit, delete product
 - send order confirmation via email
 - manage online order
-- Ajax hierarchical combobox for payment method.
-- add delivery charge outside the coverage area
 - secure reservation
-- forum for customer comments about the site
 - generates various report
 
 ## API Endpoints
@@ -43,8 +40,8 @@ POST | /product/[productId]/comment                  | Private | create a commen
 | GET | /user/[userId]/orders      | PRIVATE | Get all the order of a user
 | GET | /orders/[orderId]      | PRIVATE | GET a order 
 | GET | /orders     | PRIVATE | GET all the order 
-| PUT | /orders/[orderId]    | PRIVATE | Update a order as admin 
-| DELETE | /orders/[orderId]     | PRIVATE |Delete a order as admin 
+| PUT | /orders/[orderId]    | PRIVATE | Update a order as a user connected
+| DELETE | /orders/[orderId]     | PRIVATE |Delete a order as a user connected 
 
 
                       
--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -6,13 +6,16 @@ import { Product } from './entities/product.entity';
 import { CommentsService } from 'src/comments/comments.service';
 import { Comment } from 'src/comments/entities/comment.entity';
 import { AuthGuard } from 'src/auth/auth.guard';
+import { Roles } from 'src/users/decorators/roles.decorator';
+import { Role } from 'src/enums/role.enum';
+import { RolesGuard } from 'src/users/users.guard';
 @Controller('products')
 export class ProductsController {
   
   constructor(private readonly productsService: ProductsService,
     private readonly commentService :CommentsService) {}
-  
-  @UseGuards(AuthGuard)
+  @Roles(Role.Admin)
+  @UseGuards(AuthGuard,RolesGuard)
   @Post()
   create(@Body() product:Product): Promise<Product> {
     return this.productsService.create(product);
@@ -32,6 +35,8 @@ export class ProductsController {
   async getProductComments(@Param('productId') productId: string) {
     return this.productsService.findCommentOfProduct(parseInt(productId))
   }
+  @Roles(Role.Admin,Role.User)
+  @UseGuards(AuthGuard,RolesGuard)
   @UseGuards(AuthGuard)
   @Post(':productId/comment')
   async addCommentToProduct(
@@ -41,12 +46,14 @@ export class ProductsController {
     return this.commentService.create({...comment,product_id:productId})
   }
 
-
+  @Roles(Role.Admin)
+  @UseGuards(AuthGuard,RolesGuard)
   @Put(':id')
   update(@Param('id') id: number, @Body() product:Product): Promise<any> {
     return this.productsService.update(id, product);
   }
-
+  @Roles(Role.Admin)
+  @UseGuards(AuthGuard,RolesGuard)
   @Delete(':id')
   remove(@Param('id') id: number) {
     return this.productsService.delete(id);
